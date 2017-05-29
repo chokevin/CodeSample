b456a5af8dbcad2ffe6ab2304dda5101c2a7dd61
chokevin <cap.kevincho@gmail.com>
2017-05-29 15:58:06 -0700

    Initial setup complete

--- React/dashboard/src/index.js
+++ React/dashboard/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '../semantic/dist/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
