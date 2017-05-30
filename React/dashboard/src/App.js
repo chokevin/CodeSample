461d5de602d5bea9c1700fa0020152e8c4951fc1
chokevin <cap.kevincho@gmail.com>
2017-05-30 07:52:55 -0700

    changed README

--- React/dashboard/src/App.js
+++ React/dashboard/src/App.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
-import { Button, Icon } from 'semantic-ui-react'
-import nba from 'nba.js';
+import React, {Component} from 'react';
+import {Button, Icon} from 'semantic-ui-react'
 import logo from './logo.svg';
 import './App.css';
-
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-
                 <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={logo} className="App-logo" alt="logo"/>
                     <h2>Welcome to React</h2>
                 </div>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
+                    To get started, edit
+                    <code>src/App.js</code>
+                    and save to reload.
                 </p>
-                <ButtonExampleAnimated />
-                <NBAScore />
+                <ButtonExampleAnimated/>
             </div>
         );
     }
 }
-
 
 const ButtonExampleAnimated = () => (
     <div>
         <Button animated>
             <Button.Content visible>Next</Button.Content>
             <Button.Content hidden>
-            <Icon name='right arrow' />
+                <Icon name='right arrow'/>
             </Button.Content>
         </Button>
     </div>
