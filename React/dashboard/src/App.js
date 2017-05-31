2851d721d6e4efe3f35b6d8aaaa58a0a6f39159a
chokevin <cap.kevincho@gmail.com>
2017-05-30 19:55:39 -0700

    added clock, modularized clock into clock.js, modularized mainpage

--- React/dashboard/src/App.js
+++ React/dashboard/src/App.js
@@ -2,24 +2,29 @@ import React, {Component} from 'react';
 import {Button, Icon} from 'semantic-ui-react'
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock.js';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit
-                    <code>src/App.js</code>
-                    and save to reload.
-                </p>
+                <MainPage name={new String("Kevin")}/>
                 <ButtonExampleAnimated/>
+                <Clock date={new Date()}/>
             </div>
         );
     }
+}
+
+function MainPage(props) {
+    return (
+        <div>
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h2>Welcome {props.name}</h2>
+            </div>
+        </div>
+    )
 }
 
 const ButtonExampleAnimated = () => (
